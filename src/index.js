8919409ee6a68d4cf20f434ae05d6efbbb71c22a
Jarkko Mönkkönen <jamonkko@gmail.com>
2016-03-04 23:05:25 +0200

    First commit
    Add chain and boundChain
    Add package.json and babel conf

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import Promise from 'bluebird';
+
+function chainImpl(first, ...functions) {
+  return functions
+    .map((f) => f instanceof Function ? f : () => f)
+    .reduce((promise, f) => promise.then(f), first);
+}
+
+export default {
+  chain(...functions) {
+    return chainImpl(Promise.resolve(), ...functions);
+  },
+  boundChain(initial = {}, ...functions) {
+    let state = initial;
+    let boundFunctions = functions;
+    if (state instanceof Function) {
+      boundFunctions = [state].concat([state], functions);
+      state = {};
+    }
+    return chainImpl(Promise.resolve().bind({}), ...boundFunctions);
+  },
+};
